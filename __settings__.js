28973826ea6c86ace6f68ec4fc1a1536ce1b20c8
Luiz Henrique (Fire) <157321593+FireGodis@users.noreply.github.com>
2025-06-15 18:09:05 -0300

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2180129.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 216364319, 215967736, 216149067, 216149108, 216182683, 216326520, 216330266, 216343516, 216363575, 216344679, 216364900, 216530523, 216671901, 216713896, 216974419, 216974425, 216974466, 217133779, 217368065, 217657204, 217665508, 217673450, 217676028, 218411634, 218941970, 219476077, 219873213, 222159460, 222262406, 222897094, 228628018, 228628149, 228630819, 228639435, 228639439, 229496739, 232180341, 232181551, 232184612, 233711401, 233717395, 233717524, 233719421, 233720836, 234124830, 234543255 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/216149021/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/216149022/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/216149020/1/ammo.js', 'preload' : true},
+];
